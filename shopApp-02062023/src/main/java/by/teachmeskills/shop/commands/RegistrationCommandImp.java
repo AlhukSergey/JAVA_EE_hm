e161c9f3666b5511606014e4632e7567a7203a5f
AlhukSergey <alshuk73@gmail.com>
2023-06-17 16:44:30 +0300

    rebased on fixed hm_1606

--- shopApp-02062023/src/main/java/by/teachmeskills/shop/commands/RegistrationCommandImp.java
+++ shopApp-02062023/src/main/java/by/teachmeskills/shop/commands/RegistrationCommandImp.java
@@ -1,72 +1,59 @@
-package by.teachmeskills.shop.servlet;
+package by.teachmeskills.shop.commands;
 
-import by.teachmeskills.shop.exceptions.*;
+import by.teachmeskills.shop.commands.enums.PagesPathEnum;
+import by.teachmeskills.shop.exceptions.CommandException;
+import by.teachmeskills.shop.exceptions.IncorrectUserDataException;
+import by.teachmeskills.shop.exceptions.RequestCredentialsNullException;
 import by.teachmeskills.shop.model.Category;
 import by.teachmeskills.shop.model.User;
 import by.teachmeskills.shop.utils.CRUDUtils;
 import by.teachmeskills.shop.utils.DateParser;
 import by.teachmeskills.shop.utils.HttpRequestCredentialsValidator;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@WebServlet("/registration")
-public class RegistrationServlet extends HttpServlet {
+public class RegistrationCommandImp implements BaseCommand {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher rd = req.getRequestDispatcher("/registration.jsp");
-        rd.forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Map<String, String> credentials = new HashMap<>();
-        credentials.put("NAME", req.getParameter("name"));
-        credentials.put("SURNAME", req.getParameter("surname"));
-        credentials.put("BIRTHDAY", req.getParameter("birthday"));
-        credentials.put("EMAIL", req.getParameter("email"));
-        credentials.put("PASSWORD", req.getParameter("password"));
+    public String execute(HttpServletRequest req) throws CommandException {
+        Map<String, String> userData = new HashMap<>();
+        userData.put("NAME", req.getParameter("name"));
+        userData.put("SURNAME", req.getParameter("surname"));
+        userData.put("BIRTHDAY", req.getParameter("birthday"));
+        userData.put("EMAIL", req.getParameter("email"));
+        userData.put("PASSWORD", req.getParameter("password"));
 
         try {
-            if (HttpRequestCredentialsValidator.validateCredentials(credentials)) {
-                User user = User.newBuilder()
-                        .withId()
-                        .withName(credentials.get("NAME"))
-                        .withSurname(credentials.get("SURNAME"))
-                        .withBirthday(DateParser.parseToDate(credentials.get("BIRTHDAY")))
-                        .withEmail(credentials.get("EMAIL"))
-                        .withPassword(credentials.get("PASSWORD"))
-                        .build();
-
-                CRUDUtils.createUser(user);
-
-
-                HttpSession session = req.getSession();
-                session.setAttribute("user", user);
-
-                String varInfo = "Добро пожаловать, " + user.getName() + ".";
-                req.setAttribute("info", varInfo);
-
-                showCategories(req);
-                RequestDispatcher rd = req.getRequestDispatcher("/home.jsp");
-                rd.forward(req, resp);
-            }
-        } catch (InvalidDateFormatException | AgeLimitException | InvalidEmailException | InvalidPasswordException |
-                 RequestCredentialsNullException e) {
-            String varInfo ="Введены неверные данные. " + e.getMessage();
+            HttpRequestCredentialsValidator.validateUserData(userData);
+        } catch (IncorrectUserDataException | RequestCredentialsNullException e) {
+            String varInfo = "Введены некорректные  данные. " + e.getMessage();
             req.setAttribute("info", varInfo);
-            RequestDispatcher rd = req.getRequestDispatcher("/registration.jsp");
-            rd.forward(req, resp);
+            return PagesPathEnum.REGISTRATION_PAGE.getPath();
         }
+
+        User user = User.newBuilder()
+                .withId()
+                .withName(userData.get("NAME"))
+                .withSurname(userData.get("SURNAME"))
+                .withBirthday(DateParser.parseToDate(userData.get("BIRTHDAY")))
+                .withEmail(userData.get("EMAIL"))
+                .withPassword(userData.get("PASSWORD"))
+                .build();
+
+        CRUDUtils.createUser(user);
+
+
+        HttpSession session = req.getSession();
+        session.setAttribute("user", user);
+
+        String varInfo = "Добро пожаловать, " + user.getName() + ".";
+        req.setAttribute("info", varInfo);
+
+        showCategories(req);
+        return PagesPathEnum.HOME_PAGE.getPath();
     }
 
     private void showCategories(HttpServletRequest req) {
