19193f2f92863b7b7442adba3d95ab8a183bcaf1
AlhukSergey <alshuk73@gmail.com>
2023-07-11 12:04:24 +0300

    added MyLinkedList

---
+++ shopApp-02062023/src/main/java/by/teachmeskills/shop/MyLinkedKist/MyLinkedList.java
@@ -0,0 +1,159 @@
+package by.teachmeskills.shop.MyLinkedKist;
+
+import java.util.NoSuchElementException;
+
+public class MyLinkedList<T> {
+    private Node<T> head;
+    int size;
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(T t) {
+        //если список пустой, то элемент будет начальным, и не будет ни на кого ссылаться.
+        if (isEmpty())
+            head = new Node<>(null, t, null);
+        else {
+            //Иначе, сохраняю начальный элемент в промежуточную переменную, чтобы не потерять значение;
+            //перезаписываю начальный элемент на передаваемый в параметре и даю ему ссылку на промежуточный элемент(старый начальный);
+            //промежуточному элементу даю ссылку на новый начальный;
+            Node<T> temp = head;
+            head = new Node<>(null, t, temp);
+            head.next.prev = head;
+        }
+        size++;
+    }
+
+    public void addLast(T t) {
+        //если список пустой, то элемент будет первым.
+        if (isEmpty())
+            head = new Node<>(null, t, null);
+        else {
+            //Иначе, беру сохраняю головной элемент в промежуточную переменную, а затем перезаписываю промежуточную переменную, пока ссылка на ее следующий;
+            //элемент не будет равна null. Таким образом нахожу последний элемент в листе;
+            //Назначаю ссылку последнего листа на новый, а новому на последний;
+            Node<T> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node<>(temp, t, null);
+        }
+        size++;
+    }
+
+    public void addBefore(T o, T t) {
+        //если список пустой, то выбрасываю исключение, что данного элемента не существует;
+        if (isEmpty())
+            throw new NoSuchElementException("Element " + o.toString() + " not found.");
+
+        Node<T> currentNode = head;
+
+        //перебираю список, пока не буден найден элемент, перед которым необходимо вставить узел либо не упрусь в null;
+        while (currentNode != null && !currentNode.value.equals(o))
+            currentNode = currentNode.next;
+
+        //Если промежуточный элемент равен null, т. е. список закончился, а нужный элемент, перед которым нужно вставить новый, так и не нашелся,
+        //бросаю исключение, что данного элемента нет в списке.
+        if (currentNode == null)
+            throw new NoSuchElementException("Element " + o.toString() + " not found.");
+
+        //Создаю узел, где ссылка "предыдущий" указывает на предыдущий элемент промежуточной переменной, а "последующим" выступает сама промежуточная переменная.
+        Node<T> newNode = new Node<>(currentNode.prev, t, currentNode);
+
+        //проверка на то, что промежуточный элемент не является головным
+        if (currentNode.prev != null) {
+            currentNode.prev.next = newNode;}
+        else {
+            head = newNode;
+        currentNode.prev = newNode;}
+
+        size++;
+    }
+
+    public void addAfter(T o, T t) {
+        if (isEmpty())
+            throw new NoSuchElementException("Element " + o.toString() + " not found.");
+
+        Node<T> currentNode = head;
+
+        while (currentNode != null && !currentNode.value.equals(o))
+            currentNode = currentNode.next;
+
+        if (currentNode == null)
+            throw new NoSuchElementException("Element " + o.toString() + " not found.");
+
+        Node<T> newNode = new Node<>(currentNode, t, currentNode.next);
+
+        //проверка на то, что промежуточный элемент не является конечным.
+        if (currentNode.next != null)
+            currentNode.next.prev = newNode;
+        currentNode.next = newNode;
+
+        size++;
+
+
+    }
+
+    public void remove(T t) {
+        //если список пустой, то выбрасываю исключение, что данного элемента не существует;
+        if (isEmpty())
+            throw new NoSuchElementException("Element " + t.toString() + " not found");
+
+        //Если удаляемый элемент является головным, то просто перезаписываю его на следующий. Если в списке лишь один элемент,
+        // который удаляю, то ссылка на следующий указывает на null;
+        if (head.value.equals(t)) {
+            head = head.next;
+            return;
+        }
+
+        Node<T> currentNode = head;
+
+        //перебираю список, пока не буден найден удаляемый элемент;
+        while (currentNode != null && !currentNode.value.equals(t))
+            currentNode = currentNode.next;
+
+        //Если промежуточный элемент равен null, т. е. список закончился, а нужный элемент, перед которым нужно вставить новый, так и не нашелся,
+        //бросаю исключение, что данного элемента нет в списке.
+        if (currentNode == null)
+            throw new NoSuchElementException("Element " + t.toString() + " not found.");
+
+        //Если нужный элемент найден, но не достигнут конец списка, то последующему элементу перебиваю ссылку на предыдущий элемент текущего,
+        if (currentNode.next != null)
+            currentNode.next.prev = currentNode.prev;
+
+        currentNode.prev.next = currentNode.next;
+
+        size--;
+    }
+
+    @Override
+    public String toString() {
+        Node<T> temp = head;
+        StringBuilder builder = new StringBuilder("[");
+
+        while (temp != null) {
+            builder.append(temp.value).append(",");
+            temp = temp.next;
+        }
+        builder.deleteCharAt(builder.length() - 1);
+        builder.append("]");
+        return builder.toString();
+    }
+
+    private static class Node<T> {
+        private Node<T> prev;
+        private final T value;
+        private Node<T> next;
+
+        public Node(Node<T> prev, T value, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ shopApp-02062023/src/test/java/by/teachmeskills/MyLinkedList/MyLinkedListTest.java
@@ -0,0 +1,102 @@
+package by.teachmeskills.MyLinkedList;
+
+import by.teachmeskills.shop.MyLinkedKist.MyLinkedList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyLinkedListTest {
+    private MyLinkedList<Integer> myLinkedList;
+
+    @BeforeEach
+    public void setUp() {
+        myLinkedList = new MyLinkedList<>();
+    }
+
+    @Test
+    public void checkIsEmptyReturnTrue() {
+        assertTrue(myLinkedList.isEmpty());
+    }
+
+    @Test
+    public void checkIsEmptySizeReturnZero() {
+        assertEquals(0, myLinkedList.size());
+    }
+
+    @Test
+    public void checkRemoveNotPresentThrowsException() {
+        assertThrows(NoSuchElementException.class, () -> myLinkedList.remove(2));
+    }
+
+    @Test
+    public void checkAddBeforeNotFoundThrowsException() {
+        assertThrows(NoSuchElementException.class, () -> myLinkedList.addBefore(0, 1));
+    }
+
+    @Test
+    public void testInsertAtFront() {
+        for (int i = 5; i != -1; i--) {
+            myLinkedList.addFirst(i);
+        }
+        assertEquals("[0,1,2,3,4,5]", myLinkedList.toString());
+    }
+
+    @Test
+    public void testInsertAtEnd() {
+        for (int i = 0; i < 5; i++) {
+            myLinkedList.addLast(i);
+        }
+        assertEquals("[0,1,2,3,4]", myLinkedList.toString());
+    }
+
+    @Test
+    public void testAddAfter() {
+        for (int i = 5; i != -1; i--) {
+            myLinkedList.addFirst(i);
+        }
+
+        //[0,1,2,3,4,5]
+
+        myLinkedList.addAfter(5, 55);
+        myLinkedList.addAfter(4, 44);
+        myLinkedList.addAfter(3, 33);
+        myLinkedList.addAfter(2, 22);
+        myLinkedList.addAfter(1, 11);
+
+        assertEquals("[0,1,11,2,22,3,33,4,44,5,55]", myLinkedList.toString());
+    }
+
+    @Test
+    public void testAddBefore() {
+        for (int i = 0; i < 5; i++) {
+            myLinkedList.addFirst(i);
+        }
+
+        //[0,1,2,3,4]
+
+        myLinkedList.addBefore(4, 44);
+        myLinkedList.addBefore(3, 33);
+        myLinkedList.addBefore(2, 22);
+        myLinkedList.addBefore(1, 11);
+
+        assertEquals("[44,4,33,3,22,2,11,1,0]", myLinkedList.toString());
+    }
+
+    @Test
+    public void testRemove() {
+        for (int i = 5; i != -1; i--) {
+            myLinkedList.addFirst(i);
+        }
+
+        //[0,1,2,3,4,5]
+
+        myLinkedList.remove(2);
+
+        assertEquals("[0,1,3,4,5]", myLinkedList.toString());
+    }
+}
